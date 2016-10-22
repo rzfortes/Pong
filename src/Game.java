d6d649eac0e750fb43891790116318c92928ffea
Ricafelle Dorothy Z. Fortes <rzfortes@up.edu.ph>
2016-10-22 21:50:04 +0800

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -5,73 +5,79 @@
 
 package minitennis;
 
-
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Game extends JPanel {
-	
-	// refers to the object made with "new"
-		Ball ball = new Ball(this);
-		Racquet racquet = new Racquet(this);
-		
-		public Game() {
-			addKeyListener(new KeyListener() {
-				@Override
-				public void keyTyped(KeyEvent e) {
-					
-				}
-				
-				@Override
-				public void keyReleased(KeyEvent e) {
-					racquet.keyReleased(e);
-				}
-				
-				@Override
-				public void keyPressed(KeyEvent e) {
-					racquet.keyPressed(e);
-				}
-			});
-			// should not forget, dili mu-move ang racquet if wala ni
-			setFocusable(true);
-		}
-		
-		private void move() {
-			ball.move();
-			racquet.move();
-		}
-	    
-		@Override
-	    public void paint(Graphics g) {
-	        super.paint(g); // clears the screen
-	        Graphics2D g2d = (Graphics2D) g;
-	        // makes the borders of the figures smooth
-	        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-	        ball.paint(g2d);
-	        racquet.paint(g2d);
-	        
-	    }
     
-		public static void main(String[] args) throws InterruptedException {
-	        // the window that will frame the panel object of minitennis
-	        JFrame frame = new JFrame("Mini Tennis");
-	        Game game = new Game();
-	        frame.add(game);
-	        frame.setSize(300, 400);
-	        frame.setVisible(true);
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        
-	        while(true) {
-	            game.move(); // updates the position of the ball
-	            game.repaint(); // renders
-	            Thread.sleep(10);
-	        }
-	    }
+    Ball ball = new Ball(this);
+    Racquet racquet = new Racquet(this);
+
+    public Game() {
+        addKeyListener(new KeyListener() {
+            
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                racquet.keyReleased(e);
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                racquet.keyPressed(e);
+            }
+        });
+        // should not forget, dili mu-move ang racquet if wala ni
+        setFocusable(true);
+        Sound.BACK.loop();
+    }
+
+    private void move() {
+        ball.move();
+        racquet.move();
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g); // clears the screen
+        Graphics2D g2d = (Graphics2D) g;
+        // makes the borders of the figures smooth
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        ball.paint(g2d);
+        racquet.paint(g2d);
+    }
+    
+    public void gameOver() {
+        Sound.BACK.stop();
+        Sound.GAMEOVER.play();
+        JOptionPane.showMessageDialog(this, "Game Over", "Game Over", JOptionPane.YES_NO_OPTION);
+        System.exit(ABORT);
+    }
+    
+    public static void main(String[] args) throws InterruptedException {
+        // the window that will frame the panel object of minitennis
+        JFrame frame = new JFrame("Mini Tennis");
+        Game game = new Game();
+        frame.add(game);
+        frame.setSize(300, 400);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        while(true) {
+            game.move(); // updates the position of the ball
+            game.repaint(); // renders
+            Thread.sleep(10);
+        }
+    }
 }
