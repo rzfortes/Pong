98978823704c91a64747c5ff68baedc911cbf217
Ricafelle Dorothy Z. Fortes <rzfortes@up.edu.ph>
2016-10-22 22:05:37 +0800

    added things from tutorial 9

--- src/Game.java
+++ src/Game.java
@@ -5,6 +5,9 @@
 
 package minitennis;
 
+
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
@@ -19,6 +22,11 @@ public class Game extends JPanel {
     
     Ball ball = new Ball(this);
     Racquet racquet = new Racquet(this);
+    int speed = 1;
+    
+    private int getScore() {
+        return speed - 1;
+    }
 
     public Game() {
         addKeyListener(new KeyListener() {
@@ -56,12 +64,17 @@ public class Game extends JPanel {
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         ball.paint(g2d);
         racquet.paint(g2d);
+        
+        // paints the score
+        g2d.setColor(Color.GRAY);
+        g2d.setFont(new Font("Verdana", Font.BOLD, 30));
+        g2d.drawString(String.valueOf(getScore()), 10, 30);
     }
     
     public void gameOver() {
         Sound.BACK.stop();
         Sound.GAMEOVER.play();
-        JOptionPane.showMessageDialog(this, "Game Over", "Game Over", JOptionPane.YES_NO_OPTION);
+        JOptionPane.showMessageDialog(this, "Your score is: " + getScore(), "Game Over", JOptionPane.YES_NO_OPTION);
         System.exit(ABORT);
     }
     
