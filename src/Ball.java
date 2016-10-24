41cf5dbf97fde69e3cf111c41de509b28b0594c9
Ricafelle Dorothy Z. Fortes <rzfortes@up.edu.ph>
2016-10-24 23:02:43 +0800

    added players' score counters, updated some

--- src/Ball.java
+++ src/Ball.java
@@ -1,16 +1,21 @@
-package minitennis;
+package Pong;
 
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
 public class Ball {
     private static final int DIAMETER = 30;
-    int x = 0;
-    int y = 0;
+    // the initial position of the ball
+    int x = 50;
+    int y = 100;
     // the variables below gives us the speed and direction
     int xa = 1;
     int ya = 1;
     private Game game;
+    
+    // variables for the scores of the players
+    int player1 = 0;
+    int player2 = 0;
 	
     public Ball(Game game) {
         this.game = game;
@@ -21,39 +26,65 @@ public class Ball {
         
         boolean changeDirection = true;
         
+        // right
         if(x + xa < 0) {
-        xa = game.speed;
+            xa = game.speed;
         }
+        // left
         if(x + xa > game.getWidth() - DIAMETER) {
             xa = -game.speed;
         }
+        // if ni-lapas sa boundary ng upper racquet/player2, game over
         if(y + ya < 0) {
-            ya = game.speed;
+            game.gameOver();
         }
+        // if ni-lapas sa boundary ng player1/lower racquet, game over sad
         if(y + ya > game.getHeight() - DIAMETER) {
             game.gameOver();
         }
+        /* if nag-collide ang ball ang player1 racquet, direction is up
+            player1 score incremented, speed ng racquet incremented also
+        
+            likewise for player2, direction is down naman
+        */
         if(collision()) {
             ya = -game.speed;
             y = game.racquet.getTopY() - DIAMETER;
-            game.speed++;
+            game.speed = game.speed + 1;
+            player1++;
+        } else if(collision2()) {
+            ya = game.speed;
+            x = game.racquet2.getTopX() + DIAMETER;
+            game.speed = game.speed + 1;
+            player2++;
         } else {
             changeDirection = false;
         }
         
+        // naay bug pa
+        if(player1 >= 3||player2 >= 3){
+            game.gameOver();
+        }
+        // sound effect for the collision of ball and racquet
         if(changeDirection) {
             Sound.BALL.play();
         }
+        
         x = x + xa;
         y = y + ya;
     
     }
     
+    // checks if nag-collide ang ball and racquet
     public boolean collision() {
         return game.racquet.getBounds().intersects(getBounds());
     }
+    
+    public boolean collision2() {
+        return game.racquet2.getBounds().intersects(getBounds());
+    }
 
-    // receives the graphics2D, and paints
+    // paint ball
     public void paint(Graphics2D g){
         g.fillOval(x, y, DIAMETER, DIAMETER);
     }
