be37b78a000ea2fa427b21399ecd975952461c78
Ricafelle Dorothy Z. Fortes <rzfortes@up.edu.ph>
2016-10-24 23:04:14 +0800

    added Restart Game function, updated some

--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,7 @@
 * 10/21/2016
 */
 
-package minitennis;
+package Pong;
 
 
 import java.awt.Color;
@@ -13,6 +13,8 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -22,10 +24,16 @@ public class Game extends JPanel {
     
     Ball ball = new Ball(this);
     Racquet racquet = new Racquet(this);
+    Racquet2 racquet2 = new Racquet2(this);
+    
     int speed = 1;
     
+    // also the winner part
     private int getScore() {
-        return speed - 1;
+        if(ball.player1 > ball.player2) {
+            return ball.player1;
+        }
+        return ball.player2;
     }
 
     public Game() {
@@ -39,21 +47,25 @@ public class Game extends JPanel {
             @Override
             public void keyReleased(KeyEvent e) {
                 racquet.keyReleased(e);
+                racquet2.keyReleased(e);
             }
 
             @Override
             public void keyPressed(KeyEvent e) {
                 racquet.keyPressed(e);
+                racquet2.keyPressed(e);
             }
         });
         // should not forget, dili mu-move ang racquet if wala ni
         setFocusable(true);
         Sound.BACK.loop();
     }
-
+    
+    // moves everything
     private void move() {
         ball.move();
         racquet.move();
+        racquet2.move();
     }
     
     @Override
@@ -64,18 +76,56 @@ public class Game extends JPanel {
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         ball.paint(g2d);
         racquet.paint(g2d);
+        racquet2.paint(g2d);
         
-        // paints the score
+        // player 1 score and player 2 score, same font and color
         g2d.setColor(Color.GRAY);
         g2d.setFont(new Font("Verdana", Font.BOLD, 30));
-        g2d.drawString(String.valueOf(getScore()), 10, 30);
+        g2d.drawString(String.valueOf(ball.player2), 10, 30);
+        g2d.drawString(String.valueOf(ball.player1), 10, 350);
     }
     
     public void gameOver() {
+        // stop the sound effect of ball and racquet collision
         Sound.BACK.stop();
+        // sound effect for game over, NOTE: should change
         Sound.GAMEOVER.play();
-        JOptionPane.showMessageDialog(this, "Your score is: " + getScore(), "Game Over", JOptionPane.YES_NO_OPTION);
-        System.exit(ABORT);
+        // for displaying the which player won
+        String player;
+        if(ball.player1 > ball.player2) {
+            player = "Player 1";
+        } else {
+            player = "Player 2";
+        }
+        JOptionPane.showMessageDialog(this, player + " score is: " + getScore(), "Game Over", JOptionPane.YES_NO_OPTION);
+        
+        // restarting the game option, NOTE: NEED TO FIND OUT HOW TO DELETE OR RESET BC DUHA KA JFRAME NA
+        int n = JOptionPane.showConfirmDialog(this, "Restart game?", "Game Over", JOptionPane.YES_NO_OPTION);
+        if(n == JOptionPane.YES_OPTION){
+            try {
+                RestartGame();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            System.exit(ABORT);
+        }
+    }
+    
+    // restart the game if yes, NOTE: SHOULD FIND OUT HOW TO DELETE THE FIRST JFRAME, OR RESET MAN LANG
+    public void RestartGame() throws InterruptedException {
+        JFrame frame = new JFrame("Mini Tennis");
+        Game game = new Game();
+        frame.add(game);
+        frame.setSize(300, 400);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        while(true) {
+            game.move(); // updates the position of the ball
+            game.repaint(); // renders
+            Thread.sleep(10);
+        }
     }
     
     public static void main(String[] args) throws InterruptedException {
