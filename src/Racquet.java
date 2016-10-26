1ea85fcd84949080370f73cc1167fc360af36dd0
Ricafelle Dorothy Z. Fortes <rzfortes@up.edu.ph>
2016-10-26 23:43:49 +0800

    updated constructor, added a setter

--- src/Racquet.java
+++ src/Racquet.java
@@ -5,18 +5,22 @@ import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 
 public class Racquet {
-    private static int Y = 340;
+    private int Y; // for the position of the racquet along y axis
     private static final int WIDTH = 60;
     private static final int HEIGHT = 10;
-    int left;
-    int right;
-    // position of the racquet
+
+    // position of the racquet along x axis
     int x = 400 / 2;
     int xa = 0;
     private Game game;
 
-    public Racquet(Game game) {
+    public Racquet(Game game, int Y) {
         this.game = game;
+        this.Y = Y;
+    }
+    
+    public void setY(int Y) {
+        this.Y = Y;
     }
 
     public void move() {
@@ -32,15 +36,6 @@ public class Racquet {
     public void keyReleased(KeyEvent e) {
         xa = 0;
     }
-
-    public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-            xa = -game.speed;
-        }
-        if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            xa = game.speed;
-        }
-    }
     
     public Rectangle getBounds() {
         return new Rectangle(x, Y, WIDTH, HEIGHT);
@@ -48,6 +43,10 @@ public class Racquet {
     
     public int getTopY() {
         return Y - HEIGHT;
+    }
+    
+    public int getTopX() {
+        return x - HEIGHT;
     }
 
 }
